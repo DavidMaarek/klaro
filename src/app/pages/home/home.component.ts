4099f7d7d4962f83a3989a7023df135f6475f691
David Maarek <davidmaarek55@gmail.com>
2024-03-17 22:33:47 +0100

    Implement arrow for slider on home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { HomeService } from "../../services/home/home.service";
 import { HomeCard } from "../../interfaces/home-cards.interface";
@@ -15,6 +15,10 @@ import { HomeCardComponent } from "../../components/home-card/home-card.componen
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('klCarousel') public klCarousel: ElementRef<HTMLUListElement>;
+  @ViewChild('klCarouselCard') public klCarouselCard: ElementRef<HTMLElement>;
+  public displayLeftArrow: boolean = false
+  public displayRightArrow: boolean = false
   public primaryCards: HomeCard[] = [];
   public secondaryCards: HomeCard[] = [];
   public errorApi: boolean = false;
@@ -28,10 +32,45 @@ export class HomeComponent implements OnInit {
       next: (response: HomeCard[]): void => {
         this.primaryCards = response.filter((card: HomeCard) => card.type == "primary");
         this.secondaryCards = response.filter((card: HomeCard) => card.type == "secondary");
+        setTimeout(() => {
+          this.updateCarousel();
+        }, 200)
       },
       error: (error) => {
         this.errorApi = true;
       }
     })
+  }
+
+  @HostListener('window:resize', [''])
+  public updateCarousel(): void {
+    const klCarouselNE: HTMLUListElement = this.klCarousel.nativeElement;
+    const scrollLeft: number = klCarouselNE.scrollLeft;
+    const scrollWidth: number = klCarouselNE.scrollWidth;
+    const clientWidth: number = klCarouselNE.clientWidth;
+    const windowInnerWidth: number = window.innerWidth;
+    const carouselPadding: number = 16;
+
+    this.displayLeftArrow = scrollLeft > carouselPadding;
+    this.displayRightArrow = windowInnerWidth < scrollWidth;
+
+    // On masque la fleche de droite si
+    if(
+      // La taille de la fenêtre est < à la taille des elements du carousel
+      windowInnerWidth > scrollWidth ||
+      // Le scroll max est atteint
+      scrollLeft + clientWidth >= scrollWidth - carouselPadding
+    ) {
+      this.displayRightArrow = false;
+    }
+  }
+
+  public scroll(direction: 'left' | 'right'): void {
+    const scrollChange: number = direction === 'left' ? -this.klCarouselCard.nativeElement.offsetWidth : this.klCarouselCard.nativeElement.offsetWidth;
+    this.klCarousel.nativeElement.scroll({
+      left: this.klCarousel.nativeElement.scrollLeft + scrollChange,
+      top: 0,
+      behavior: 'smooth',
+    });
   }
 }
