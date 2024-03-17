a442c0cf9bd9982848a559c2ab2f41647044231e
David Maarek <davidmaarek55@gmail.com>
2024-03-17 18:44:22 +0100

    Remove useless import

--- src/app/components/home-card/home-card.component.ts
+++ src/app/components/home-card/home-card.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { HomeCard } from "../../interfaces/home-cards.interface";
-import { NgClass } from "@angular/common";
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home-card',
   standalone: true,
   imports: [
-    NgClass,
     RouterLink
   ],
   templateUrl: './home-card.component.html',
